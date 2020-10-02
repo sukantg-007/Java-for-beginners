21479a6e95d8112d72925e0d490c1a0b1139eb73
sukantg-007 <72221521+sukantg-007@users.noreply.github.com>
2020-10-02 22:33:33 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -4,10 +4,10 @@ public class Factorial {
 
         int num = 10;
         long factorial = 1;
-        for(int i = 1; i <= num; ++i)
+        for(int i = 1; i <= num;)
         {
             // factorial = factorial * i;
-            factorial *= i;
+            factorial *= i++;
         }
         System.out.printf("Factorial of %d = %d", num, factorial);
     }
